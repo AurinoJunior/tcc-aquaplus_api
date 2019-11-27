b2ffa98c89855ee39bcfe82a8fc0371b06ad2b73
Aurino Geraldo <aurino_aj@hotmail.com>
2019-11-27 10:27:26 -0300

    Temperatura

--- index.js
+++ index.js
@@ -2,21 +2,35 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-// const five = require('johnny-five')
-// const Board = new five.Board()
+const { Board, Thermometer } = require("johnny-five");
+const board = new Board()
 
-app.use(cors())
-app.use(express.json())
+board.on("ready", () => {
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Tudo ok' })
-})
+  const thermometer = new Thermometer({
+    controller: "DS18B20",
+    pin: 2
+  });
 
-app.get('/temperatura', (req, res) => {
-  const temp = '60'
-  res.send({ temperatura: temp })
-})
+  app.use(cors())
+  app.use(express.json())
 
-app.listen(3333, () => {
-  console.log('Rodando na porta 3333')
+  app.get('/', (req, res) => {
+    res.send({ message: 'Tudo ok' })
+  })
+
+  app.get('/temperatura', (req, res) => {
+    const { celsius } = thermometer;
+    const graus = celsius.toPrecision(2)
+    console.log(graus)
+    res.set('Content-Type', 'application/json').send({ temperatura: graus })
+  })
+
+  app.get('/cooler', (req, res) => {
+    res.send({ message: 'cooler ligado' })
+  })
+
+  app.listen(3333, () => {
+    console.log('Rodando na porta 3333')
+  })
 })
