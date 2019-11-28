a1f0ef08611f1152997be85fb4dce15f23dfdf05
Aurino Geraldo <aurino_aj@hotmail.com>
2019-11-28 04:04:09 -0300

    Criando codigo do modulo rele

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('events').EventEmitter.defaultMaxListeners = 15;
 
-const { Board, Thermometer } = require("johnny-five");
+
+const { Board, Thermometer, Sensor, Relay } = require("johnny-five");
 const board = new Board()
 
 board.on("ready", () => {
 
+  /* Sensores */
   const thermometer = new Thermometer({
     controller: "DS18B20",
     pin: 2
   });
 
+  // const nivelAgua = new Sensor({
+  //   pin: "A0",
+  //   freq: 250,
+  //   threshold: 5
+  // })
+
+
+  /* ROTAS */
   app.use(cors())
   app.use(express.json())
 
@@ -19,18 +30,85 @@ board.on("ready", () => {
     res.send({ message: 'Tudo ok' })
   })
 
+
+  app.post('/relay', (req, res) => {
+    const { toggle, tipoComponente } = req.body
+    let relay;
+    switch (tipoComponente) {
+      case 'Filtro':
+        relay = new Relay(10)
+        ativaRele(relay, toggle)
+        break;
+
+      case 'Termostato':
+        relay = new Relay(10)
+        ativaRele(relay, toggle)
+        break;
+
+      case 'Cooler':
+        relay = new Relay(10)
+        ativaRele(relay, toggle)
+        break;
+
+      case 'Repositor':
+        relay = new Relay(10)
+        ativaRele(relay, toggle)
+        break;
+
+      case 'Alimentador':
+        relay = new Relay(10)
+        ativaRele(relay, toggle)
+        break;
+
+      case 'Iluminacao':
+        relay = new Relay(10)
+        ativaRele(relay, toggle)
+        break;
+
+      default:
+        break;
+    }
+
+
+
+    return res.send({ toggle })
+  })
+
   app.get('/temperatura', (req, res) => {
-    const { celsius } = thermometer;
-    const graus = celsius.toPrecision(2)
-    console.log(graus)
-    res.set('Content-Type', 'application/json').send({ temperatura: graus })
+    try {
+      const { celsius } = thermometer
+      const graus = celsius.toPrecision(2)
+      console.log(graus)
+      return res.set('Content-Type', 'application/json').send({ temperatura: graus })
+    } catch (e) {
+      console.error('Deu ruim com a temperatura')
+    }
   })
 
-  app.get('/cooler', (req, res) => {
-    res.send({ message: 'cooler ligado' })
-  })
+  // app.get('/nivelagua', (req, res) => {
+  //   try {
+  //     console.log("Agua", nivelAgua.scaleTo(0, 1))
+  //     return res.set('Content-Type', 'application/json')
+  //       .send({ nivel: 1 })
+  //   } catch (e) {
+  //     console.error('Deu ruim com o nivel de agua')
+  //   }
+  // })
 
+  /* Server rodando */
   app.listen(3333, () => {
     console.log('Rodando na porta 3333')
   })
 })
+
+/*Func Utils*/
+
+function ativaRele(relay, toggle) {
+  if (toggle) {
+    relay.on()
+    console.log('Relay open')
+  } else {
+    console.log('Relay close')
+    relay.off()
+  }
+}
